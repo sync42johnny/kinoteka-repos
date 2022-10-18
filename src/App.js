8c71a46f7dbb1db4843c2826f1599b29a69204f3
Nikita <zurtis228@gmail.com>
2022-10-18 22:42:09 +0300

    create movies component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Counter } from "./features/counter/Counter";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Movies from "./components/Movies";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <Movies />
     </div>
   );
 }
---
+++ src/components/Movies.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Movies() {
+  return (
+    <Container>
+        <h4>Recommended for You</h4>
+    </Container>
+  )
+}
+
+export default Movies
+
+const Container = styled.div``
