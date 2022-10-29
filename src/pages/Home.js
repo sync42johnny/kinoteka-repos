60d80996ff3424a1fe782a7d3179021503b91b61
ivb3989 <73277168+ivb3989@users.noreply.github.com>
2022-10-29 16:46:15 +0300

    move home page to pages folder

---
+++ src/pages/Home.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+import ImgSlider from "../components/ImgSlider";
+import Movies from "../components/Movies";
+
+function Home() {
+  return (
+    <Container>
+      <ImgSlider />
+      <Movies />
+    </Container>
+  );
+}
+
+export default Home;
+
+const path = process.env.PUBLIC_URL;
+
+const Container = styled.main`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+  overflow-x: hidden;
+
+  &:before {
+    background: url(${path + "/images/home-background.png"}) center center /
+      cover no-repeat fixed;
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
+`;
