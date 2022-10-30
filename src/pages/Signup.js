691c6ece1097ccb4dedaa4725184784c8be0e8c4
ivb3989 <73277168+ivb3989@users.noreply.github.com>
2022-10-30 10:20:13 +0200

    modify empty signup component

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,28 +1,146 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import BackgroundImage from "../components/BackgroundImage";
+import Header from "../components/Header";
 
 function Signup() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+
+  const handleSignIn = async () => {
+    try {
+      const { email, password } = formValues;
+      /*  */
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <Container>
-        <BackgroundImage />
+    <Container showPassword={showPassword}>
+      <BackgroundImage />
+      <div className="content">
+        <Header login />
         <div className="body flex column a-center j-center">
-            <div className="text flex column">
-                <h1>Unlimited movies, TV shows and more</h1>
-                <h4>Watch anywhere.</h4>
-                <h6>Ready to watch? Enter your email to create membership</h6>
-            </div>
-            <div className="form">
-                <input type="emial" placeholder="Email Address" name="email" />
-                <input type="password" placeholder="Password" name="password" />
-                <button>Get Started</button>
-            </div>
-            <button>Log In</button>
+          <div className="text flex column">
+            <h1>Unlimited movies, TV shows and more</h1>
+            <h4>Watch anywhere.</h4>
+            <h6>Ready to watch? Enter your email to create membership</h6>
+          </div>
+          <div className="form">
+            <input
+              type="emial"
+              placeholder="Email Address"
+              name="email"
+              value={formValues.email}
+              onChange={(e) =>
+                setFormValues({
+                  ...formValues,
+                  [e.target.name]: e.target.value,
+                })
+              }
+            />
+            {showPassword && (
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={formValues.password}
+                onChange={(e) =>
+                  setFormValues({
+                    ...formValues,
+                    [e.target.name]: e.target.value,
+                  })
+                }
+              />
+            )}
+            {!showPassword && (
+              <button onClick={() => setShowPassword(true)}>Get Started</button>
+            )}
+          </div>
+          {showPassword && <button onClick={handleSignIn}>Sign Up</button>}
         </div>
+      </div>
     </Container>
   );
 }
 
 export default Signup;
 
-const Container = styled.div``;
+const Container = styled.div`
+  position: relative;
+  .content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    height: 100vh;
+    width: 100vw;
+    display: grid;
+    grid-template-rows: 15vh 85vh;
+
+    .body {
+      gap: 1rem;
+      .text {
+        gap: 1rem;
+        text-align: center;
+        font-size: 2rem;
+        h1 {
+          padding: 0 25rem;
+        }
+      }
+      .form {
+        display: grid;
+        grid-template-columns: ${({ showPassword }) =>
+          showPassword ? "1fr 1fr" : "2fr 1fr"};
+        width: 60%;
+
+        input {
+          color: black;
+          border: none;
+          padding: 1.5rem;
+          font-size: 1.2rem;
+          border-radius: 4px;
+
+          &:focus {
+            outline: none;
+          }
+        }
+
+        button {
+          background-color: #0063e5;
+          padding: 0.5rem 1rem;
+          border: none;
+          text-align: center;
+          font-size: 1.05rem;
+          cursor: pointer;
+          transition: all 250ms;
+          letter-spacing: 1.5px;
+          text-transform: uppercase;
+
+          &:hover {
+            background: #0483ee;
+          }
+        }
+      }
+      button {
+        background-color: #0063e5;
+        padding: 0.5rem 1rem;
+        border: none;
+        border-radius: 4px;
+        text-align: center;
+        font-size: 1.05rem;
+        cursor: pointer;
+        transition: all 250ms;
+        letter-spacing: 1.5px;
+        text-transform: uppercase;
+      }
+    }
+  }
+`;
