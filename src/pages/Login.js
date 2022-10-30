99a15c60cb446b3c6f204ec6cc89d77d2f1b6e0c
ivb3989 <73277168+ivb3989@users.noreply.github.com>
2022-10-30 10:19:40 +0200

    modify empty login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,116 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import BackgroundImage from "../components/BackgroundImage";
+import Header from "../components/Header";
 
 function Login() {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleLogIn = async () => {
+    try {
+      const { email, password } = formValues;
+      /*  */
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Container>
+      <BackgroundImage />
+      <div className="content">
+        <Header />
+        <div className="form-container flex flex-column a-center j-center">
+          <div className="form flex column a-center j-center">
+            <div className="title">
+              <h3>Login</h3>
+            </div>
+            <div className="container flex column">
+              <input
+                type="emial"
+                placeholder="Email Address"
+                name="email"
+                value={formValues.email}
+                onChange={(e) =>
+                  setFormValues({
+                    ...formValues,
+                    [e.target.name]: e.target.value,
+                  })
+                }
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={formValues.password}
+                onChange={(e) =>
+                  setFormValues({
+                    ...formValues,
+                    [e.target.name]: e.target.value,
+                  })
+                }
+              />
+              <button onClick={handleLogIn}>Log In</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
 }
 
 export default Login;
+
+const Container = styled.div`
+  position: relative;
+  .content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    height: 100vh;
+    width: 100vw;
+    display: grid;
+    grid-template-rows: 15vh 85vh;
+    .form-container {
+      gap: 2rem;
+      height: 85vh;
+      .form {
+        padding: 2rem;
+        background-color: #00000b0;
+        width: 25vw;
+        gap: 2rem;
+        color: white;
+        .container {
+          gap: 2rem;
+          input {
+            padding: 0.5rem 1rem;
+            width: 15rem;
+          }
+          button {
+            background-color: #0063e5;
+            padding: 0.5rem 1rem;
+            border: none;
+            border-radius: 4px;
+            text-align: center;
+            font-size: 1.05rem;
+            cursor: pointer;
+            transition: all 250ms;
+            letter-spacing: 1.5px;
+            text-transform: uppercase;
+
+            &:hover {
+              background: #0483ee;
+            }
+          }
+        }
+      }
+    }
+  }
+`;
