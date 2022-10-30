7cd26cf37f11dd5dd68be8734f74d15247461cb5
ivb3989 <73277168+ivb3989@users.noreply.github.com>
2022-10-30 10:14:42 +0200

    create background component

--- src/components/BackgroundImage.js
+++ src/components/BackgroundImage.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 function BackgroundImage() {
+  const path = process.env.PUBLIC_URL;
+
   return (
-    <div>BackgroundImage</div>
-  )
+    <Container>
+      <img
+        src={path + "/images/brian-mcgowan-ggg_B1MeqQk-unsplash.jpg"}
+        alt="background"
+      />
+    </Container>
+  );
 }
 
-export default BackgroundImage
+export default BackgroundImage;
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  img {
+    height: 100vh;
+    width: 100vw;
+    object-fit: cover;
+  }
+`;
