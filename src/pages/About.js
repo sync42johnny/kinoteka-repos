323e859c654ec43690f9e198aa003955e5931a31
ivb3989 <73277168+ivb3989@users.noreply.github.com>
2022-11-01 19:03:27 +0200

    modify css variables

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,18 @@
 import React from "react";
 import styled from "styled-components";
+import ImgSlider from "../components/ImgSlider";
+import Navbar from "../components/Navbar";
 
 function About() {
   return (
     <Container>
+      <Navbar />
+      <Content>
         <CTA>
-            <CTALogo src={path + "/images/kinoteka.svg"} />
-            <SiteDesctiption>
-            GET ALL THERE
-            </SiteDesctiption>
+          <CTALogo src={path + "/images/kinoteka.svg"} />
+          <SiteDesctiption>GET ALL THERE</SiteDesctiption>
         </CTA>
+      </Content>
     </Container>
   );
 }
@@ -18,53 +21,65 @@ const path = process.env.PUBLIC_URL;
 export default About;
 
 const Container = styled.main`
-position: relative;
-height: calc(100vh - 70px);
-display: flex;
-align-items: center;
-justify-content:center;
-&:before {
-    background-position: top;
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-image: url(${path+"/images/brian-mcgowan-LObpG0ku8VM-unsplash.jpg"});
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Content = styled.div`
+  position: relative;
+  min-height: calc(100vh - 70px);
+  overflow-x: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 70px;
+  padding: 0 calc(3.5vw + 5px);
+  &:after {
+    background: url(${path + "/images/brian-mcgowan-ggg_B1MeqQk-unsplash.jpg"})
+      center center / cover no-repeat fixed;
     content: "";
-    opacity: 0.8;
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -2;
+  }
+  &:before {
+    content: "";
     position: absolute;
     top: 0;
     left: 0;
-    right: 0;
-    bottom: 0;
-    z-index:-1;
-}
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    background: rgba(0, 0, 0, 0.6);
   }
-`
+`;
+
 const CTA = styled.div`
-max-width: 650px;
-padding: 80px 40px;
-display:flex;
-flex-direction: column;
-margin-bottom: 10%;
-width: 650px;
-`
+  max-width: 650px;
+  padding: 80px 40px;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10%;
+  width: 650px;
+`;
 const CTALogo = styled.img`
-padding: 10px 0px;
-`
+  padding: 10px 0px;
+`;
 const SiteDesctiption = styled.a`
   width: 100%;
   background-color: #0063e5;
   font-weight: bold;
   padding: 15px 5px;
   border-radius: 4px;
-  text-align:center;
+  text-align: center;
   font-size: 18px;
   cursor: pointer;
   transition: all 250ms;
   letter-spacing: 1.5px;
   margin-top: 8px;
   margin-bottom: 12px;
-
-  &:hover{
+  &:hover {
     background: #0483ee;
   }
-`
+`;
