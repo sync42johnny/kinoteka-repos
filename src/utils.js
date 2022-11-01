955dbf1941926c57251cf289957eb979c5fc73a2
Nikita <zurtis228@gmail.com>
2022-11-01 19:28:48 +0200

    setup utils

---
+++ src/utils.js
@@ -0,0 +1,14 @@
+export const carouselImages = [
+  {
+    id: "1",
+    url: "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/A38B478F4FFCBF16886C14872D13CB64EC615E97B380028FC51162EC61495800/scale?width=1440&aspectRatio=3.91&format=jpeg",
+  },
+  {
+    id: "2",
+    url: "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/A38B478F4FFCBF16886C14872D13CB64EC615E97B380028FC51162EC61495800/scale?width=1440&aspectRatio=3.91&format=jpeg",
+  },
+  {
+    id: "3",
+    url: "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/A38B478F4FFCBF16886C14872D13CB64EC615E97B380028FC51162EC61495800/scale?width=1440&aspectRatio=3.91&format=jpeg",
+  },
+];
