e8ed30d78f2d3f2d591fe4a35c7e77abc1ba96f9
Nikita <zurtis228@gmail.com>
2022-11-01 19:22:01 +0200

    modify slider component

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -1,38 +1,46 @@
-import React from "react";
 import styled from "styled-components";
-import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { carouselImages } from "../utils";
+import Slider from "react-slick";
 
-function ImgSlider() {
-  const path = process.env.PUBLIC_URL;
+const ImgSlider = (props) => {
   let settings = {
     dots: true,
     infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    speed: 400,
+    slideToShow: 1,
+    slideToScroll: 1,
     autoplay: true,
   };
   return (
     <Carousel {...settings}>
-      <Wrap>
-        <img  src={path + "/images/thumbnail-bg.png"} />
-      </Wrap>
-      <Wrap>
-        <img  src={path + "/images/thumbnail-bg.png"} />
-      </Wrap>
-      <Wrap>
-        <img  src={path + "/images/thumbnail-bg.png"} />
-      </Wrap>
+      {carouselImages.map((image) => (
+        <Wrap key={image.id}>
+          <a>
+            <img src={image.url} alt={image.name} />
+          </a>
+        </Wrap>
+      ))}
     </Carousel>
   );
-}
+};
 
-export default ImgSlider;
-
+// carousel custom style
 const Carousel = styled(Slider)`
   margin-top: 20px;
+
+  & > button {
+    opacity: 0;
+    height: 100%;
+    width: 5vw;
+    z-index: 1;
+
+    &:hover {
+      opacity: 1;
+      transition: opacity 0.2s ease 0s;
+    }
+  }
 
   ul li button {
     &:before {
@@ -46,28 +54,43 @@ const Carousel = styled(Slider)`
   }
 
   .slick-list {
-    overflow: visible;
+    overflow: initial;
   }
 
-  button {
-    z-index: 1;
+  .slick-prev {
+    left: -75px;
+  }
+
+  .slick-next {
+    right: -75px;
   }
 `;
 
+// wrap style
 const Wrap = styled.div`
+  border-radius: 4px;
+  position: relative;
   cursor: pointer;
 
-  img {
-    border: 4px solid transparent;
-    border-radius: 4px;
-    width: 100%;
-    height: 100%;
+  a {
     box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
       rgb(0 0 0 / 73%) 0px 16px 10px -10px;
-    transition-duration: 300ms;
+    border-radius: 4px;
+    cursor: pointer;
+    display: block;
+    position: relative;
+    padding: 4px;
 
+    img {
+      width: 100%;
+      height: 100%;
+    }
     &:hover {
-      border: 4px solid rgba(249, 249, 249, 0.8);
+      padding: 0px;
+      border: 4px solid rgb(249, 249, 249, 0.8);
+      transition-duration: 300ms;
     }
   }
 `;
+
+export default ImgSlider;
