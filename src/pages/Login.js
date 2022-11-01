a0b53101c04a2022819590dff43f4c4e6f883227
ivb3989 <73277168+ivb3989@users.noreply.github.com>
2022-11-01 18:49:37 +0200

    set up authentication

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,32 +3,31 @@ import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import BackgroundImage from "../components/BackgroundImage";
 import Header from "../components/Header";
-//import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
-//import { firebaseAuth } from "../utils/firebase-config";
+import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { firebaseAuth } from "../utils/firebase-config";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [userName, setUserName] = useState("");
   const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
-      //await signInWithEmailAndPassword(firebaseAuth, email, password);
-      navigate("/");
+      await signInWithEmailAndPassword(firebaseAuth, email, password);
     } catch (error) {
       console.log(error.code);
     }
   };
 
-  /* onAuthStateChanged(firebaseAuth, (currentUser) => {
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
     if (currentUser) navigate("/");
-  }); */
+  });
 
   return (
     <Container>
-      <BackgroundImage />
-      <div className="content">
-        <Header />
+      <Header />
+      <Content>
         <div className="form-container flex column a-center j-center">
           <div className="form flex column a-center j-center">
             <div className="title">
@@ -51,12 +50,87 @@ function Login() {
             </div>
           </div>
         </div>
-      </div>
+      </Content>
     </Container>
   );
 }
 
+const path = process.env.PUBLIC_URL;
+
+// container section
 const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+// content div
+const Content = styled.div`
+  position: relative;
+  min-height: calc(100vh - 70px);
+  overflow-x: hidden;
+  display: grid;
+  padding: 0 calc(2.7vw + 5px);
+
+  &:after {
+    background: url(${path + "/images/login-background.jpg"}) center center /
+      cover no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+
+  .form-container {
+    gap: 2rem;
+    height: 85vh;
+
+    .form {
+      padding: 2rem;
+      width: 25vw;
+      gap: 2rem;
+      color: white;
+
+      .container {
+        max-width: 100%;
+        display: flex;
+        flex-direction: column;
+        margin: auto;
+
+        input {
+          width: 100%
+          font-size: 1.3rem;
+          padding: 1.2rem;
+          border-radius: 5px;
+          border: 0;
+          margin-bottom: 2px;
+        }
+
+        button {
+          background-color: rgba(0, 0, 0, 0.6);
+          margin: 20px 0;
+          padding: 1.2rem;
+          letter-spacing: 1.5px;
+          text-decoration: none;
+          text-transform: uppercase;
+          border-radius: 4px;
+          cursor: pointer;
+          vertical-align: middle;
+          display: inline-block;
+          border: 1px solid #f9f9f9;
+          transition: all 0.2s ease 0s;
+
+          &:hover {
+            background-color: #f9f9f9;
+            color: #000;
+            border-color: transparent;
+          }
+        }
+      }
+    }
+  }
+`;
+
+/* const Container = styled.div`
   position: relative;
   .content {
     position: absolute;
@@ -71,23 +145,31 @@ const Container = styled.div`
       height: 85vh;
       .form {
         padding: 2rem;
-        background-color: #000000b0;
         width: 25vw;
         gap: 2rem;
         color: white;
         .container {
-          gap: 2rem;
           input {
-            padding: 0.5rem 1rem;
-            width: 15rem;
+            margin-bottom: 10px;
+            color: black;
+            border: none;
+            padding: 0.8rem;
+            font-size: 1.2rem;
+            appearance: none;
+            border: 1px solid #8c8c8c;
+            border-radius: 2px;
+            box-sizing: border-box;
+            &:focus {
+              outline: none;
+            }
           }
           button {
+            border-radius: 2px;
             padding: 0.5rem 1rem;
             background-color: #e50914;
             border: none;
             cursor: pointer;
             color: white;
-            border-radius: 0.2rem;
             font-weight: bolder;
             font-size: 1.05rem;
           }
@@ -95,6 +177,6 @@ const Container = styled.div`
       }
     }
   }
-`;
+`; */
 
 export default Login;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import BackgroundImage from "../components/BackgroundImage";
 import Header from "../components/Header";
-//import { firebaseAuth } from "../utils/firebase-config";
+import { firebaseAuth } from "../utils/firebase-config";
 function Signup() {
   const [showPassword, setShowPassword] = useState(false);
   const [formValues, setFormValues] = useState({
@@ -19,26 +19,23 @@ function Signup() {
   const handleSignIn = async () => {
     try {
       const { email, password } = formValues;
-      //await createUserWithEmailAndPassword(firebaseAuth, email, password);
-      navigate("/");
+      await createUserWithEmailAndPassword(firebaseAuth, email, password);
     } catch (error) {
       console.log(error);
     }
   };
 
-  /* onAuthStateChanged(firebaseAuth, (currentUser) => {
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
     if (currentUser) navigate("/");
-  }); */
+  });
 
   return (
     <Container showPassword={showPassword}>
-      <BackgroundImage />
-      <div className="content">
-        <Header login />
+      <Header login />
+      <Content>
         <div className="body flex column a-center j-center">
           <div className="text flex column">
             <h1>Unlimited movies, TV shows and more.</h1>
-            <h4>Watch anywhere. Cancel anytime.</h4>
             <h6>
               Ready to watch? Enter your email to create or restart membership.
             </h6>
@@ -74,68 +71,105 @@ function Signup() {
               <button onClick={() => setShowPassword(true)}>Get Started</button>
             )}
           </div>
-          {showPassword && <button onClick={handleSignIn}>Log In</button>}
+          {showPassword && <button onClick={handleSignIn}>Sign up</button>}
         </div>
-      </div>
+      </Content>
     </Container>
   );
 }
 
+const path = process.env.PUBLIC_URL;
+
+// container section
 const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+// content div
+const Content = styled.div`
   position: relative;
-  .content {
+  min-height: calc(100vh - 70px);
+  overflow-x: hidden;
+  display: grid;
+  padding: 0 calc(2.7vw + 5px);
+
+  &:after {
+    background: url(${path + "/images/login-background.jpg"}) center center /
+      cover no-repeat fixed;
+    content: "";
     position: absolute;
-    top: 0;
-    left: 0;
-    background-color: rgba(0, 0, 0, 0.5);
-    height: 100vh;
-    width: 100vw;
-    display: grid;
-    grid-template-rows: 15vh 85vh;
-    .body {
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+
+  .body {
+    gap: 1rem;
+    .text {
       gap: 1rem;
-      .text {
-        gap: 1rem;
-        text-align: center;
-        font-size: 2rem;
-        h1 {
-          padding: 0 25rem;
-        }
+      text-align: center;
+      font-size: 2rem;
+      h1 {
+        padding: 0 25rem;
       }
-      .form {
-        display: grid;
-        grid-template-columns: ${({ showPassword }) =>
-          showPassword ? "1fr 1fr" : "2fr 1fr"};
-        width: 60%;
-        input {
-          color: black;
-          border: none;
-          padding: 1.5rem;
-          font-size: 1.2rem;
-          border: 1px solid black;
-          &:focus {
-            outline: none;
-          }
-        }
-        button {
-          padding: 0.5rem 1rem;
-          background-color: #e50914;
-          border: none;
-          cursor: pointer;
-          color: white;
-          font-weight: bolder;
-          font-size: 1.05rem;
+    }
+    .form {
+      display: grid;
+      grid-template-columns: ${({ showPassword }) =>
+        showPassword ? "1fr 1fr" : "2fr 1fr"};
+      width: 60%;
+      input {
+        color: black;
+        border: none;
+        padding: 1.5rem;
+        font-size: 1.2rem;
+        appearance: none;
+        border: 1px solid #8c8c8c;
+        border-radius: 2px;
+        box-sizing: border-box;
+        &:focus {
+          outline: none;
         }
       }
       button {
+        border-radius: 2px;
         padding: 0.5rem 1rem;
-        background-color: #e50914;
-        border: none;
-        cursor: pointer;
-        color: white;
-        border-radius: 0.2rem;
         font-weight: bolder;
         font-size: 1.05rem;
+        background-color: rgba(0, 0, 0, 0.6);
+        letter-spacing: 1.5px;
+        text-decoration: none;
+        text-transform: uppercase;
+        cursor: pointer;
+        display: inline-block;
+        border: 1px solid #f9f9f9;
+        transition: all 0.2s ease 0s;
+
+        &:hover {
+          background-color: #e50914;
+          color: #fff;
+          border-color: transparent;
+        }
+      }
+    }
+    button {
+      border-radius: 2px;
+      padding: 0.5rem 1rem;
+      background-color: rgba(0, 0, 0, 0.6);
+      letter-spacing: 1.5px;
+      text-decoration: none;
+      text-transform: uppercase;
+      display: inline-block;
+      border: 1px solid #f9f9f9;
+      transition: all 0.2s ease 0s;
+      cursor: pointer;
+      font-weight: 400;
+      font-size: 1.05rem;
+
+      &:hover {
+        background-color: #e50914;
+        color: #fff;
+        border-color: transparent;
       }
     }
   }
