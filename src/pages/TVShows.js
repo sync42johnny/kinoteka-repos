f91f3369754a52f9e3fd47b359e27cfb57849842
Nikita <zurtis228@gmail.com>
2022-11-16 18:34:09 +0200

    create TVShows page

--- src/pages/TVShows.js
+++ src/pages/TVShows.js
@@ -1,44 +1,79 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-//import { onAuthStateChanged } from "firebase/auth";
-//import { firebaseAuth } from "../utils/firebase-config";
+import { onAuthStateChanged } from "firebase/auth";
+import { firebaseAuth } from "../utils/firebase-config";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchMovies, getGenres } from "../store";
 import SelectGenre from "../components/SelectGenre";
+import Navbar from "../components/Navbar";
+import Section from "../components/Trending";
 
 function TVShows() {
+  const [isScrolled, setIsScrolled] = useState(false);
   const movies = useSelector((state) => state.netflix.movies);
   const genres = useSelector((state) => state.netflix.genres);
   const genresLoaded = useSelector((state) => state.netflix.genresLoaded);
-  const dataLoading = useSelector((state) => state.netflix.dataLoading);
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (!genres.length) dispatch(getGenres());
+    dispatch(getGenres());
   }, []);
 
   useEffect(() => {
     if (genresLoaded) {
       dispatch(fetchMovies({ genres, type: "tv" }));
+      console.log(movies);
     }
   }, [genresLoaded]);
 
   const [user, setUser] = useState(undefined);
 
-  /* onAuthStateChanged(firebaseAuth, (currentUser) => {
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
     if (currentUser) setUser(currentUser.uid);
     else navigate("/login");
-  }); */
+  }); 
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
 
   return (
     <Container>
+      <Navbar isScrolled={isScrolled} />
+      <div className="data">
+        <SelectGenre genres={genres} type="tv" />
+        {movies.length ? (
+          <>
+            <Section movies={movies} />
+          </>
+        ) : (
+          <h1 className="not-available">
+            No TV Shows avaialble for the selected genre. Please select a
+            different genre.
+          </h1>
+        )}
+      </div>
     </Container>
   );
 }
 
 const Container = styled.div`
+padding: 0 8rem;
+@media (max-width: 1024px){
+  padding: 0 4rem;
+}
+@media (max-width: 768px){
+  padding: 0 2rem;
+}
+.data {
+  margin-top: 8rem;
+  .not-available {
+    text-align: center;
+    margin-top: 4rem;
+  }
+}
 `;
 export default TVShows;
