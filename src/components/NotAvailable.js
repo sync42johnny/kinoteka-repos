e4b10a9bd83b963c939114745633b8974d2b22c4
Nikita <zurtis228@gmail.com>
2022-11-16 19:07:08 +0200

    modify notavalliable component

--- src/components/NotAvailable.js
+++ src/components/NotAvailable.js
@@ -3,8 +3,7 @@ import React from "react";
 export default function NotAvailable() {
   return (
     <h1 className="not-available">
-      No Movies avaialble for the selected genre. Please select a different
-      genre.
+      No Movies avaialble for the selected genre. Please select a different genre.
     </h1>
   );
 }
