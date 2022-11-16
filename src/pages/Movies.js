75247080ab8efb635fe68e5825d786bd0e1337e7
Nikita <zurtis228@gmail.com>
2022-11-16 18:35:01 +0200

    create Movies page

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-//import { onAuthStateChanged } from "firebase/auth";
-//import { firebaseAuth } from "../utils/firebase-config";
+import { onAuthStateChanged } from "firebase/auth";
+import { firebaseAuth } from "../utils/firebase-config";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchMovies, getGenres } from "../store";
 import SelectGenre from "../components/SelectGenre";
+import Navbar from "../components/Navbar";
+import Slider from "../components/Trending";
 import NotAvailable from "../components/NotAvailable";
 
 function MoviePage() {
+  const [isScrolled, setIsScrolled] = useState(false);
   const movies = useSelector((state) => state.netflix.movies);
   const genres = useSelector((state) => state.netflix.genres);
   const genresLoaded = useSelector((state) => state.netflix.genresLoaded);
@@ -23,22 +26,49 @@ function MoviePage() {
   useEffect(() => {
     if (genresLoaded) {
       dispatch(fetchMovies({ genres, type: "movie" }));
+      console.log(movies);
     }
   }, [genresLoaded]);
 
   const [user, setUser] = useState(undefined);
 
-  /* onAuthStateChanged(firebaseAuth, (currentUser) => {
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
     if (currentUser) setUser(currentUser.uid);
     else navigate("/login");
-  }); */
+  });
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
 
   return (
     <Container>
+      <div className="navbar">
+        <Navbar isScrolled={isScrolled} />
+      </div>
+      <div className="data">
+        <SelectGenre genres={genres} type="movie" />
+        {movies.length ? <Slider movies={movies} /> : <NotAvailable />}
+      </div>
     </Container>
   );
 }
 
 const Container = styled.div`
+padding: 0 8rem;
+@media (max-width: 1024px){
+  padding: 0 4rem;
+}
+@media (max-width: 768px){
+  padding: 0 2rem;
+}
+.data {
+  margin-top: 8rem;
+  .not-available {
+    text-align: center;
+    color: white;
+    margin-top: 2rem;
+  }
+}
 `;
 export default MoviePage;
