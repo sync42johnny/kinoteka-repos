7bf43c593b8f17d5e94e4a6d5321dcc6c95b2950
ivb3989 <boretskiy7.open1@gmail.com>
2022-12-04 15:03:50 +0200

    add global provider

--- src/App.js
+++ src/App.js
@@ -10,20 +10,26 @@ import Header from "./components/Header";
 import About from "./pages/About";
 import CardDetails from "./components/CardDetails";
 import Navbar from "./components/Navbar";
+import AddMovie from "./components/AddMovie";
+import { GlobalProvider } from "./context/GlobalState";
+import Watchlist from "./components/Watchlist";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/carddetails/:id" element={<CardDetails />} />
-        <Route exact path="/about" element={<About />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/signup" element={<Signup />} />
-        <Route exact path="/series" element={<TVShows />} />
-        <Route exact path="/movies" element={<Movies />} />
-        <Route exact path="/mylist" element={<UserListedMovies />} />
-        <Route exact path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <GlobalProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/carddetails/:id" element={<CardDetails />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/series" element={<TVShows />} />
+          <Route exact path="/movies" element={<Movies />} />
+          <Route exact path="/mylist" element={<Watchlist />} />
+          <Route exact path="/addmovie" element={<AddMovie />} />
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </GlobalProvider>
   );
 }
