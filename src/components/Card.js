26c1e2023a77d33541f15da6d61cab44f81f39d7
ivb3989 <boretskiy7.open1@gmail.com>
2022-12-04 15:04:58 +0200

    fix component

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,8 +6,9 @@ import styled from "styled-components";
 //import { firebaseAuth } from "../utils/firebase-config";
 import { useDispatch } from "react-redux";
 //import { removeMovieFromLiked } from "../store";
+import MovieControls from "./MovieControls";
 
-export default React.memo(function Card({ index, movieData }) {
+export default React.memo(function Card({ index, movieData, type }) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   //const [email, setEmail] = useState(undefined);
@@ -28,20 +29,46 @@ export default React.memo(function Card({ index, movieData }) {
       console.log(error);
     }
   }; */
+  console.log(movieData);
 
   return (
     <Container>
       <Link to={`/carddetails/` + movieData.id}>
         <img
-          src={`https://image.tmdb.org/t/p/w500${movieData.image}`}
+          src={`https://image.tmdb.org/t/p/w500${
+            movieData.image ? movieData.image : movieData.backdrop_path
+          }`}
           alt={movieData.name}
         />
       </Link>
+      {type && (
+        <DropDown>
+          <MovieControls type={type} movie={movieData} />
+        </DropDown>
+      )}
     </Container>
   );
 });
 
+const DropDown = styled.div`
+  position: absolute;
+  bottom: 0px;
+  right: 0px;
+  left: 0px;
+  background: rgb(19, 19, 19);
+  border: 1px solid rgba(151, 151, 151, 0.34);
+  border-radius: 4px;
+  box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0px;
+  padding: 5px;
+  font-size: 14px;
+  letter-spacing: 3px;
+  opacity: 0;
+  padding: 0 auto;
+`;
+
 const Container = styled.div`
+  position: relative;
+
   padding-top: 56.25%;
   border-radius: 12px;
   box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
@@ -52,6 +79,13 @@ const Container = styled.div`
   transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
   border: 3px solid rgba(249, 249, 249, 0.1);
 
+  &:hover {
+    ${DropDown} {
+      opacity: 1;
+      transition-duration: 1s;
+    }
+  }
+
   img {
     inset: 0px;
     display: block;
@@ -61,10 +95,9 @@ const Container = styled.div`
     position: absolute;
     transition: opacity 500ms ease-in-out 0s;
     width: 100%;
-    z-index: 1;
+    z-index: ;
     top: 0;
   }
-
   &:hover {
     box-shadow: rgb(0 0 0 / 81%) 0px 41px 59px -16px,
       rgb(0 0 0 / 73%) 0px 31px 23px -11px;
